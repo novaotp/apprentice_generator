7873479da084ce11c23a2836c4beb4adc40aa801
novaotp <94069791+novaotp@users.noreply.github.com>
2023-03-25 21:19:15 +0100

    Bug hotfix

--- FICHIERS040/modules/user_options/parametres.py
+++ FICHIERS040/modules/user_options/parametres.py
@@ -1,6 +1,6 @@
 from modules.utils import *
 
-def show_settings(fast_setup_apprentice_generation_count, fast_setup_filename, fast_setup_should_overwrite):
+def show_settings():
     """
 
     Affiche certains paramètres modifiables à l'utilisateur
@@ -8,6 +8,20 @@ def show_settings(fast_setup_apprentice_generation_count, fast_setup_filename, f
     """
 
     clear_terminal()
+
+    configs = read_config()
+
+    fast_setup_apprentice_generation_count = configs.get("fast_setup_apprentice_generation_count")
+    fast_setup_filename = configs.get("fast_setup_filename")
+    fast_setup_should_overwrite = configs.get("fast_setup_should_overwrite")
+
+    if fast_setup_apprentice_generation_count is None or fast_setup_filename is None or fast_setup_should_overwrite is None or csv_folder is None:
+        sys.exit("System error : missing or incorrect config values")
+
+    try:
+        fast_setup_apprentice_generation_count = int(fast_setup_apprentice_generation_count)
+    except:
+        sys.exit("System error : missing or incorrect config values")
 
     print("================== PARAMÈTRES ==================")
     print()
