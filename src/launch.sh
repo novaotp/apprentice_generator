474480d9f01b36ba62122341afcbdf1abd0ec5f1
novaotp <94069791+novaotp@users.noreply.github.com>
2023-03-26 21:09:51 +0200

    Add files via upload

--- src/launch.sh
+++ src/launch.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Give execute permission to the app executable
-chmod +x apprentice_generator_gui
-
-# Give read and write permission to the config directory and its contents
-chmod -R +rw config
-
 # Get the current working directory
 CWD="$(pwd)"
 
 # Navigate to the directory where the script is located
 cd "$CWD"
+
+# Give execute permission to the app executable
+chmod +x apprentice_generator_gui
+
+# Give read and write permission to the config directory and its contents
+chmod -R +rw config
 
 # Check if the build was successful before launching the executable
 if [ -f "./apprentice_generator_gui" ]; then
